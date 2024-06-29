ecbeafcdbf1765068102d4a13af615e90b939e75
DiscoverBaseCode <DiscoverBaseCode@gmail.com>
2024-06-29 23:48:45 +0330

    🎉 Add Container Service In Infrastracture

---
+++ Blog.Module/Infrastructure/BlogContainerServices.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Module.Persistence.BlogModuleContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+
+namespace Blog.Module.Infrastructure;
+
+public static class BlogContainerServices
+{
+    public static IServiceCollection BlogContextRegister(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BlogContext>(option =>
+            option.UseSqlServer(configuration.GetConnectionString("BlogConnectionString")));
+
+        return services;
+    }
+
+
+    public static IServiceCollection PackagesRegister(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+
+}
