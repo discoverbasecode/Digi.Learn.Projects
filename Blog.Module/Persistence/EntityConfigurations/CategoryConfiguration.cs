4d907b8f64bd6e012410d0906bf19d2f6a47ae50
DiscoverBaseCode <DiscoverBaseCode@gmail.com>
2024-06-29 23:50:17 +0330

    🎉 Add Category Configuration

---
+++ Blog.Module/Persistence/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Module.Domain.CategoryAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Module.Persistence.EntityConfigurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.ToTable("Categories");
+
+        builder.Property(c => c.Title).HasMaxLength(300).IsRequired();
+        builder.Property(c => c.Icon).HasMaxLength(1000).IsRequired();
+        builder.Property(c => c.Sluge).HasMaxLength(1500).IsRequired();
+    }
+}
