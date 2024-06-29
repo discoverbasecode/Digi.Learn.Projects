cd46eedb81f8dc66452055b5bfe952279dc89c71
DiscoverBaseCode <DiscoverBaseCode@gmail.com>
2024-06-29 23:21:11 +0330

    🔔 Add Entity Category in Domain

---
+++ Blog.Module/Domain/CategoryAggregate/Category.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Core.Domain.Entities;
+
+namespace Blog.Module.Domain.CategoryAggregate;
+
+public class Category : BaseEntity
+{
+    public string Title { get; private set; }
+    public string Icon { get; private set; }
+    public string Sluge { get; private set; }
+
+}
