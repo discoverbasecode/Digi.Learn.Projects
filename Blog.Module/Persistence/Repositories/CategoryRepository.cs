e9a2b014eecbc97819696ed3e9af74c957b4ccfa
DiscoverBaseCode <DiscoverBaseCode@gmail.com>
2024-06-30 22:57:09 +0330

    💡 implemanted CategoryRepositpory In Persistence

---
+++ Blog.Module/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Blog.Module.Domain.CategoryAggregate;
+using Blog.Module.Persistence.BlogModuleContext;
+using Framework.Core.Persistence.Repositories;
+
+namespace Blog.Module.Persistence.Repositories;
+
+public class CategoryRepository(BlogContext context)
+    : BaseRepository<Category, BlogContext>(context), ICategoryRepository;
